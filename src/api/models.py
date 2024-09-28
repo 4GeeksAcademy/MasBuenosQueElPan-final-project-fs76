e639d17f504d8e9b5ceafc97a5d1ec60fa18c0b2
Alicia Benavente <aliciabenaventegarcia@gmail.com>
2024-09-28 11:02:05 +0000

    Producer: Get producer, get producer, get producer+id, post producer working. To test delete

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,32 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+
+class Producer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(300), unique=True, nullable=False)
+    password = db.Column(db.String(300), unique=False, nullable=False)
+    brand_name = db.Column(db.String(120), unique=True, nullable=False)
+    cif = db.Column(db.String(120), unique=True, nullable=False)
+    address = db.Column(db.String(300), unique=False, nullable=False)
+    province = db.Column(db.String(120), unique=False, nullable=False)
+    zip_code = db.Column(db.Integer, unique=False, nullable=False)
+    phone = db.Column(db.Integer, unique=True, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "brand_name": self.brand_name,
+            "cif": self.cif,
+            "address": self.address,
+            "province": self.province,
+            "zip_code": self.zip_code,
+            "phone": self.phone,
+            # do not serialize the password, its a security breach
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Producer
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +25,92 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/producer', methods=['POST'])
+def handle_signup():
+    email = request.json["email"]
+    password = request.json["password"]
+    brand_name = request.json["brand_name"]
+    cif = request.json["cif"]
+    address = request.json["address"]
+    province = request.json["province"]
+    zip_code = request.json["zip_code"]
+    phone = request.json["phone"]
+    print(email,password)
+
+    new_producer = Producer(
+        email=email,
+        password=password,
+        brand_name=brand_name,
+        cif=cif,
+        address=address,
+        province=province,
+        zip_code=zip_code,
+        phone=phone,
+        is_active=True
+        )
+    
+    print("New producer", new_producer)
+
+    db.session.add(new_producer)
+    db.session.commit()
+
+    return jsonify({"message": "User created successfully!"}), 201
+
+
+@api.route('/producers', methods=['GET'])
+def get_producers():
+    producers = Producer.query.order_by(Producer.email).all()
+    result = list(map(lambda producers: producers.serialize(), producers))
+
+    return jsonify(result), 200
+
+
+
+@api.route('/producer', methods=['GET'])
+def handle_login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    producer = Producer.query.filter_by(email=email).first() 
+    print("email",email)
+    print("password",password)
+    print("producer",producer)
+
+    if producer is None:
+        print("Incorrect email or email does not exist")
+        return jsonify({"msg": "Email  incorrect"}), 401
+    if producer.password != password:
+        print("incorrect password")
+        return jsonify({"msg": "password incorrect"}), 401
+    print(producer)
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+@api.route('/producer/<int:producer_id>', methods=['GET'])
+def get_producer(producer_id):
+    producer = Producer.query.filter_by(id=producer_id).first()
+    if producer is None:
+        return jsonify("ERROR: Error getting account"), 404
+    else:
+        return jsonify(producer.serialize()), 200
+    
+@api.route('/producer/<int:producer_id>', methods=['DELETE'])
+def delete_producer(producer_id):
+
+    producer = Producer.query.filter_by(id=producer_id).first()
+    if producer is None:
+        return jsonify("ERROR: Could not delete the account. Maybe it doesn't exist"), 404
+
+    db.session.delete(producer)
+    db.session.commit()
+
+    return jsonify(producer.serialize()), 200
